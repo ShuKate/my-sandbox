3766fc8cfcfa1ed2fb6455cb818fec6347db6a63
ShuKate <62203193+ShuKate@users.noreply.github.com>
2020-05-13 20:27:11 +0300

    Update p3.py

--- p3.py
+++ p3.py
@@ -1,4 +1,5 @@
-#calculating the area of ​​a triangle
+#calculating the area of a triangle. На вход программе подаются целые числа, выводом программы должно являться вещественное
+#число, соответствующее площади треугольника.
 a = int(input('First leg\n'))
 b = int(input('Second leg\n'))
 c = int(input('Hypotenuse\n'))
